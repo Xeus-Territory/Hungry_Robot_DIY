52d0eb78063181f379fb26cf4cb454de6073e4c9
XeusNguyen <kobikiu810@gmail.com>
2022-05-15 09:08:17 +0700

    Update module bluetooth

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <string.h>
 #include <pitch.h>
 #define SERVO_PIN 9 // Servo pin create in PWM mode
 #define TRIGGER_PIN 5
@@ -10,8 +11,11 @@
 Servo myservo;
 unsigned long time;
 extern volatile unsigned long timer0_millis; // Get the obj timer0_millis from hardware/cores/arduino/wiring.c
-bool first = true; // first time (Prevent the servo to move when the program starts)
-int rhythmNotes[] = { 
+bool first = true;                           // first time (Prevent the servo to move when the program starts)
+int distance_to_object = 5; //default distance
+unsigned int time_delay = 3000; // default time to servo
+unsigned int time_adding_delay = 3501; // default time to servo
+int rhythmNotes[] = {
     NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
     NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
     NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
@@ -33,7 +37,7 @@ int rhythmNotes[] = {
     NOTE_C5, NOTE_A4, NOTE_B4, 0,
 
     NOTE_A4, NOTE_A4,
-    //Repeat of first part
+    // Repeat of first part
     NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
     NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
     NOTE_A4, NOTE_G4, NOTE_A4, 0,
@@ -52,7 +56,7 @@ int rhythmNotes[] = {
     NOTE_D5, NOTE_E5, NOTE_A4, 0,
     NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
     NOTE_C5, NOTE_A4, NOTE_B4, 0,
-    //End of Repeat
+    // End of Repeat
 
     NOTE_E5, 0, 0, NOTE_F5, 0, 0,
     NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
@@ -85,7 +89,7 @@ int rhythmBeats[] = {
     125, 125, 375, 375,
 
     250, 125,
-    //Rpeat of First Part
+    // Rpeat of First Part
     125, 125, 250, 125, 125,
     125, 125, 250, 125, 125,
     125, 125, 375, 125,
@@ -104,7 +108,7 @@ int rhythmBeats[] = {
     250, 125, 250, 125,
     125, 125, 250, 125, 125,
     125, 125, 375, 375,
-    //End of Repeat
+    // End of Repeat
 
     250, 125, 375, 250, 125, 375,
     125, 125, 125, 125, 125, 125, 125, 125, 375,
@@ -117,14 +121,12 @@ int rhythmBeats[] = {
     125, 125, 125, 125, 125, 500};
 const float songSpeed = 1.0;
 
-
 void playMusic()
 {
   int rhythmDuration;
   const int rhythmLength = sizeof(rhythmNotes) / sizeof(int);
   for (int i = 0; i < rhythmLength; i++)
   {
-    Serial.println("Rhythm Notes: " + String(rhythmNotes[i]));
     rhythmDuration = rhythmBeats[i] / songSpeed;
     if (rhythmNotes[i] == 0)
     {
@@ -135,6 +137,10 @@ void playMusic()
       tone(BELL_PIN, rhythmNotes[i], rhythmDuration);
     }
     delay(rhythmDuration);
+    if (Serial.available() > 0)
+    {
+      break;
+    }
   }
 }
 
@@ -148,7 +154,7 @@ void setup()
   pinMode(BELL_PIN, OUTPUT);
   pinMode(INTERRUPT_PIN, INPUT_PULLUP);
   myservo.attach(SERVO_PIN);
-  myservo.write(140);
+  myservo.write(145);
 }
 
 float scan_obj()
@@ -160,9 +166,6 @@ float scan_obj()
   digitalWrite(TRIGGER_PIN, LOW);
   long duration = pulseIn(ECHO_PIN, HIGH);
   float distance = duration * 0.034 / 2;
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
   return distance;
 }
 
@@ -185,42 +188,73 @@ void setMillis(unsigned long new_millis)
 void loop()
 {
   // put your main code here, to run repeatedly:
-  Serial.println("State: " + String(digitalRead(INTERRUPT_PIN)));
-  // Prevent problems related with button press
-  if (digitalRead(INTERRUPT_PIN) == LOW)
+  while (Serial.available() > 0)
   {
-    delay(500);
-    if (digitalRead(INTERRUPT_PIN) == LOW)
+    int method = 0;
+    String x = Serial.readString();
+    if (strstr(x.c_str(), "L") != NULL)
     {
-      Serial.println("Interrupt");
-      playMusic();
-      delay(500);
+      x = x.substring(1);
+      distance_to_object = x.toInt();
     }
-  }
-  else
-  {
-    Serial.println("No Interrupt");
-    float distance = scan_obj(); // scan object
-    if (distance < 5.0 && first == true)
+    if (strstr(x.c_str(), "T") != NULL)
     {
-      first = false;
-      return;
+      x = x.substring(1);
+      time_delay = x.toInt();
+      time_adding_delay = time_delay + 501;
     }
-    Serial.println("Time: " + String(time));
-    Serial.println("Current time: " + String(millis()));
-    if (distance < 5.0 && first != true && (unsigned long)(millis() - time) > 3000)
+    if (strstr(x.c_str(), "L") == NULL && strstr(x.c_str(), "T") == NULL)
+    {
+      method = x.toInt();
+    }
+    switch (method)
+    {
+    case 1:
     {
       ring_Bell();
       myservo.write(40);
       delay(500);
-      myservo.write(140);
-      time = 0;
-      setMillis(0);
+      myservo.write(145);
+      delay(500);
     }
-    if ((unsigned long)(millis() - time) > 3501)
+    break;
+
+    case 2:
+      playMusic();
+      break;
+
+    case 3:
     {
-      setMillis(0);
+      while (true)
+      {
+        float distance = scan_obj(); // scan object
+        Serial.print(String(distance_to_object) + " " + String(time_delay) + " " + String(distance) + " ");
+        if (distance < distance_to_object && first == true)
+        {
+          first = false;
+          continue;
+        }
+        if (distance < distance_to_object && first != true && (unsigned long)(millis() - time) > time_delay)
+        {
+          ring_Bell();
+          myservo.write(40);
+          delay(500);
+          myservo.write(145);
+          time = 0;
+          setMillis(0);
+        }
+        if ((unsigned long)(millis() - time) > time_delay)
+        {
+          setMillis(0);
+        }
+        delay(500);
+        if (Serial.available() > 0)
+        {
+          break;
+        }
+      }
     }
-    delay(500);
+    break;
+    }
   }
 }
